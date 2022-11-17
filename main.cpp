3200bf43695b9641659b2ff838ea3b45c266a5bc
Aryan Panpalia <aryanpanpalia@gmail.com>
2022-11-17 09:43:37 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "FEHLCD.h"
+#include <iostream>
+
+using namespace std;
+
+/* Entry point to the application */
+int main() {
+    cout << "Hello World" << endl;
+
+    return 0;
+}
---
+++ run.bat
@@ -0,0 +1,2 @@
+C:\MinGW\mingw32\bin\mingw32-make.exe
+game.exe
