bfd25e727ad9fa6f55e9dab55e78f4134a6948bf
Aryan Panpalia <aryanpanpalia@gmail.com>
2022-11-17 09:50:15 -0500

    Create menu display

--- main.cpp
+++ main.cpp
@@ -1,11 +1,90 @@
 #include "FEHLCD.h"
 #include <iostream>
 
+// Defines the window width and height
+#define WINDOW_WIDTH 320
+#define WINDOW_HEIGHT 240
+
 using namespace std;
+using namespace FEHIcon;
+
+void DrawMenu() {
+    // Icons for all the menu options
+    Icon playButton;
+    char playString[] = "Play Game";
+    
+    Icon viewStatistics;
+    char statsString[] = "View Statistics";
+
+    Icon viewInstructions;
+    char instructionsString[] = "View Instructions";
+
+    Icon viewCredits;
+    char creditsString[] = "View Credits";
+
+    // Setting button width and height as constants
+    int buttonWidth = 220;
+    int buttonHeight = 40;
+    
+    // All buttons start at the same X position
+    int buttonStartX = (WINDOW_WIDTH - buttonWidth) / 2;
+
+    // Initial Y positions for all the buttons
+    int playStartY = 25, statsStartY = 75, instructionsStartY = 125, creditsStartY = 175;
+
+    // Sets properties of all the buttons
+    playButton.SetProperties(playString, buttonStartX, playStartY, buttonWidth, buttonHeight, GREEN, WHITE);
+    viewStatistics.SetProperties(statsString, buttonStartX, statsStartY, buttonWidth, buttonHeight, BLUE, WHITE);
+    viewInstructions.SetProperties(instructionsString, buttonStartX, instructionsStartY, buttonWidth, buttonHeight, WHITE, BLACK);
+    viewCredits.SetProperties(creditsString, buttonStartX, creditsStartY, buttonWidth, buttonHeight, RED, WHITE);
+
+    // Clears the screen then draws all the buttons
+    LCD.Clear();
+
+    LCD.SetFontColor(GREEN);
+    LCD.FillRectangle(buttonStartX, playStartY, buttonWidth, buttonHeight);
+    playButton.Draw();
+
+    LCD.SetFontColor(BLUE);
+    LCD.FillRectangle(buttonStartX, statsStartY, buttonWidth, buttonHeight);
+    viewStatistics.Draw();
+
+    LCD.SetFontColor(WHITE);
+    LCD.FillRectangle(buttonStartX, instructionsStartY, buttonWidth, buttonHeight);
+    viewInstructions.Draw();
+
+    LCD.SetFontColor(RED);
+    LCD.FillRectangle(buttonStartX, creditsStartY, buttonWidth, buttonHeight);
+    viewCredits.Draw();
+
+    int running = 1;
+    float x, y;
+    while(running){
+        // Waits for someone to touch the screen
+        while(!LCD.Touch(&x, &y));
+
+        // Waits for someone to release their touch
+        while(LCD.Touch(&x, &y));
+
+        // Depending on where the touch was at the end, do the following actions
+        if(x >= buttonStartX && x <= buttonStartX + buttonWidth){
+            if(y >= playStartY && y <= playStartY + buttonHeight){
+                cout << "Play\n";
+            } else if(y >= statsStartY && y <= statsStartY + buttonHeight){
+                cout << "Stats\n";
+            } else if(y >= instructionsStartY && y <= instructionsStartY + buttonHeight){
+                cout << "Instructions\n";
+            } else if(y >= creditsStartY && y <= creditsStartY + buttonHeight){
+                cout << "Credits\n";
+            }
+        }
+    }
+}
 
 /* Entry point to the application */
 int main() {
-    cout << "Hello World" << endl;
+    // Draws the menu
+    DrawMenu();
 
     return 0;
 }
